ef10dc4ecf57a2d71cb56f1d070733f8e784cc7c
muniba431 <munibaaaaa937@gmail.com>
2025-11-01 22:39:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const toggleBtn = document.getElementById('modeToggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  toggleBtn.textContent = document.body.classList.contains('dark-mode') ? 'light' : 'dark';
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
